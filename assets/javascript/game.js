0ac7159358b20234e98bfe7f85363ba07b7275be
JoeCeran <JoeCeran.Joecerankowski@gmail.com>
2018-11-20 17:33:43 -0500

    Second

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,56 +1,89 @@
+
+
 var score = 0;
 var round = Math.floor(Math.random() * 7);
 var i = 0;   
 var words = ["usa", "japan", "italy", "germany", "england", "china", "scotland"];
 const lengths = words.length;
 var invalid = ["0","1","2","3","4","5","6","7","8","9","enter","[","]","tab","capslock", 
-"shift","control","alt"];
+"shift","control","alt", ".", "," , "/", "-", "=", "'",";", "backspace", "`"];
 var used = [];
 var usedwrong = [];
 var guesses = 6 ; 
 var index;
 var j = round;
 
+//printing out the underscores
 function underDash(){
 if (i < words[j].length) {
         for (i = 0; i < words[j].length; i++) {
-                var targetDiv = document.getElementById("current");
-                var newDiv = document.createElement("div");
-                newDiv.textContent = "_";
-                newDiv.setAttribute("class", "dash");
-                newDiv.setAttribute("id", "current" + (i+1))
-                targetDiv.appendChild(newDiv);
+                var targetP = document.getElementById("current");
+                var newP = document.createElement("div");
+                newP.textContent = "_";
+                newP.setAttribute("class", "dash");
+                newP.setAttribute("id", "current" + (i+1))
+                targetP.appendChild(newP);
         } 
 }
 }
 
-function rounds(){
 //Selecting which flag to show
+function rounds(){
 if (round == 0) {
-        document.getElementById("img").src="assets/Images/USA.png";
+        document.getElementById("img").src="assets/images/USA.png";
         }
         else if (round == 1){
-        document.getElementById("img").src="assets/Images/Japan.png";
+        document.getElementById("img").src="assets/images/Japan.png";
         }
         else if (round == 2){
-        document.getElementById("img").src="assets/Images/Italy.png";
+        document.getElementById("img").src="assets/images/Italy.png";
         }
         else if (round == 3){
-        document.getElementById("img").src="assets/Images/Germany.png";
+        document.getElementById("img").src="assets/images/Germany.png";
         }
         else if (round == 4){
-        document.getElementById("img").src="assets/Images/England.png";
+        document.getElementById("img").src="assets/images/England.png";
         }
         else if (round == 5){
-        document.getElementById("img").src="assets/Images/China.png";
+        document.getElementById("img").src="assets/images/China.png";
         }
         else if (round == 6){
-        document.getElementById("img").src="assets/Images/Scotland.png";
+        document.getElementById("img").src="assets/images/Scotland.png";
         }
+}
+
+function winRound() {
+if (used.length == words[j].length){
+        round = Math.floor(Math.random() * 7);
+        underDash();
+        score++;
+        for(i = 0; i < words[j].length; i++){
+        used.splice(i);
+        }
+        for(i = 0; i < words[j].length; i++){
+        usedwrong.splice(i);
+        }
+        words = ["usa", "japan", "italy", "germany", "england", "china", "scotland"];
+        document.getElementById("Text").innerHTML = words[j] + " was correct!";
+        guesses = 6; 
+        for(i = 0; i < words[j].length; i++){
+        document.getElementById("current" + (i+1)).remove();
+        }
+        i = 0;
+        return score;
+        }
+}
+
+function winGame() {
+        document.getElementById("Text").innerHTML = "You Win!";
+        document.getElementById("img").src="assets/images/Win.png";
+        new Audio("assets/sounds/Woohoo.wav").play
+        return false;
 }
 
 
 
+       
 
 document.onkeyup = function(event) {
 
@@ -62,13 +95,13 @@ document.onkeyup = function(event) {
         }
 
         //Win the grand prize
-        if (score >= 6) {
-        document.getElementById("Text").innerHTML = "You Win!";
-        document.getElementById("img").src="Images/Win.png";
-        //new Audio().play
-        return false;
+        if (score > 1 ) {
+                document.getElementById("Text").innerHTML = "You Win!";
+                document.getElementById("img").src="assets/images/Win.png";
+                new Audio("assets/sounds/Woohoo.wav").play
+                return false;
         }
-
+                //compares the key to letters of the round's country flag
                 if (words[j].indexOf(letter) > -1) {
                         var x = words[j].indexOf(letter) + 1;
                         var y = 0;
@@ -76,56 +109,39 @@ document.onkeyup = function(event) {
                         used.push(letter);
                         words[j] = words[j].replace(letter," ")
                         }
-                
+                //compares the key input to see if the key is invalid
                 else if (invalid.indexOf(letter) > -1) {
-                        alert("please enter letters only!");
                 }   
+                //compares the key input to see if the key has been used
                 else if (used.indexOf(letter) > -1) {
                         return false;
-                }               
+                }  
+                //compares the key input to see if the key has been used     
                 else if (usedwrong.indexOf(letter) > -1) {
                         return false;
                 }
+                //compares the key input to see if the input is not correct
                 else  {
                         usedwrong.push(letter);
                         guesses = guesses - 1;
                 } 
 
-        //Declaring a victory      
-        if (used.length == words[j].length){
-                score++;
-                for(i = 0; i < words[j].length; i++){
-                used.splice(i);
-                }
-                for(i = 0; i < words[j].length; i++){
-                usedwrong.splice(i);
-                }
-                words = ["usa", "japan", "italy", "germany", "england", "china", "scotland"];
-                guesses = 6; 
-                for(i = 0; i < words[j].length; i++){
-                document.getElementById("current" + (i+1)).remove();
-                }
-                i = 0;
-                round = Math.floor(Math.random() * 7);
-                return score;
-        }
+        //Declaring a victory    
+        winRound();
 
         //Declaring a loss
         if (guesses < 1) {
                 alert("you lose!");   
         }
-
-        rounds();
+        
+        //Deciding what word and flag will be displayed
+        rounds();    
 
         document.getElementById("score").innerHTML = "Score: " + "<br>" + "<br>" + score;
         document.getElementById("guessesleft").innerHTML = "Guesses left: " + "<br>" + "<br>" + guesses;
-        //document.getElementById("current").innerHTML = "Current Word: ";
+        //document.getElementById("current").innerHTML = "Current Word: " ;
         document.getElementById("latter").innerHTML = "Letters already used: " + usedwrong;
-        
-}
-
-function main() {
-underDash();
-rounds();
 
 }
+
+
